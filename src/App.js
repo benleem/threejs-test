d76f6ff5aa78dea0991d5e2f7f9fe152b037bea5
Ben Marshall <benleem98@yahoo.com>
2022-01-26 17:31:26 -0500

    made earth rotate instead of camera for lighting

--- src/App.js
+++ src/App.js
@@ -2,29 +2,23 @@ import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars} from '@react-three/drei';
 import Earth from './components/Earth';
-import Pin from './components/Pin';
 import Loading from './components/Loading';
 import InputCard from './components/InputCard';
 
 const App = () => {
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
-
-    // useEffect(() => {
-    //     console.log(latitude,longitude);
-    // }, [latitude, longitude]);
+    const [latitude, setLatitude] = useState('18.2208');
+    const [longitude, setLongitude] = useState('-66.5901');
     
     return (
         <div className='App'>
             <Suspense fallback={Loading()}>
                 <InputCard setLatitude={setLatitude} setLongitude={setLongitude}/>
                 <Canvas style={{height:'100vh'}} >
-                    <OrbitControls autoRotate={true} autoRotateSpeed={.7} zoomSpeed={.7} minDistance={1.5} maxDistance={3}/>
+                    <OrbitControls zoomSpeed={.6} minDistance={1.5} maxDistance={2}/>
                     <ambientLight intensity={.5}/>
                     <spotLight position={[100, 15, 15]} angle={0.3} intensity={2} color='rgb(247,245,207)'/>
                     <Stars/>
-                    <Earth/>
-                    <Pin lat={latitude} lon={longitude}/>
+                    <Earth lat={latitude} lon={longitude}/>
                 </Canvas>
             </Suspense>
         </div>
--- src/components/Earth.js
+++ src/components/Earth.js
@@ -1,14 +1,27 @@
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
+import Pin from "./Pin";
 import earthImg from '../textures/earth-map.jpeg';
 import bumpImg from '../textures/height-map.jpeg';
 
-const Earth = () => {
+const Earth = ({ lat, lon }) => {
     const [map, bumpMap] = useTexture([earthImg, bumpImg]);
+    const earth = useRef();
+
+    useFrame(() => {
+        earth.current.rotation.y += 0.003
+    })
+      
     return(
-        <mesh receiveShadow={true} castShadow={true}>
-            <sphereBufferGeometry args={[1, 30, 30]}/>
-            <meshStandardMaterial map={map} bumpMap={bumpMap} bumpScale={.05} roughness={1} metalness={0}/>
-        </mesh>
+        <group ref={earth}>
+            <mesh receiveShadow={true} castShadow={true}>
+                <sphereBufferGeometry args={[1, 30, 30]}/>
+                <meshStandardMaterial map={map} bumpMap={bumpMap} bumpScale={.08} roughness={1} metalness={0}/>
+            </mesh>
+            <Pin lat={lat} lon={lon}/>
+        </group>
+        
     );
 };
 
